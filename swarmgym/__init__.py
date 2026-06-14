91b2a758ce703de560ed5e9e7d53e93893679614
kbh1795-sketch <kbh1795@gmail.com>
2026-06-14 19:18:20 +0900

    Add package init

---
+++ swarmgym/__init__.py
@@ -0,0 +1,5 @@
+"""SwarmGym: multi-agent reinforcement learning environments for robot swarms."""
+
+from swarmgym.envs.cooperative_search import CooperativeSearchEnv
+
+__all__ = ["CooperativeSearchEnv"]
